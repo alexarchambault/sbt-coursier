fb0abe183c0399db55f2c69087ffb47ee754057d
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-09-15 11:42:30 +0200

    TEST merge test groups

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -19,35 +19,19 @@ lmCoursier() {
 }
 
 runLmCoursierTests() {
-  if [ "$TEST_GROUP" = 1 ]; then
-    SCRIPTED_EXTRA="sbt-lm-coursier/*"
-  elif [ "$TEST_GROUP" = 2 ]; then
-    SCRIPTED_EXTRA="scala-211/*"
-  else
-    SCRIPTED_EXTRA=""
-  fi
-
   # publishing locally to ensure shading runs fine
   ./metadata/scripts/with-test-repo.sh $SBT \
     evictionCheck \
     compatibilityCheck \
     lm-coursier-shaded/publishLocal \
     lm-coursier/test \
-    "sbt-lm-coursier/scripted shared-$TEST_GROUP/* $SCRIPTED_EXTRA"
+    "sbt-lm-coursier/scripted shared-1/* shared-2/* sbt-lm-coursier/* scala-211/*"
 }
 
 runSbtCoursierTests() {
-  if [ "$TEST_GROUP" = 1 ]; then
-    SCRIPTED_EXTRA="sbt-coursier/*"
-  elif [ "$TEST_GROUP" = 2 ]; then
-    SCRIPTED_EXTRA="scala-211/*"
-  else
-    SCRIPTED_EXTRA=""
-  fi
-
   ./metadata/scripts/with-test-repo.sh $SBT \
     sbt-coursier-shared/test \
-    "sbt-coursier/scripted shared-$TEST_GROUP/* $SCRIPTED_EXTRA"
+    "sbt-coursier/scripted shared-1/* shared-2/* sbt-coursier/* scala-211/*"
 }
 
 
